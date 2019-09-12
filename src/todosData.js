45992bcdac766df1e5fb1b36d778481509433ddf
William Willems <william.willems@gmail.com>
2019-09-12 11:53:28 +0200

    initial commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "First",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Second",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Third",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Fourth",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Fifth",
+        completed: false
+    }
+]
+
+export default todosData
